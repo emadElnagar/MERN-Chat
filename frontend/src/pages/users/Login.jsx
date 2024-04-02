ac0b58d8e336d90dc4f95597ef8521607fedf866
unknown <emad.m.alnagar@gmail.com>
2024-04-03 00:18:49 +0200

    Navigate after login and register

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -1,42 +1,55 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiSolidShow, BiSolidHide } from "react-icons/bi";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Login } from '../../features/UserFeatures';
+import LoadingScreen from '../../components/LoadingScreen';
+import ErrorBox from "../../components/ErrorBox";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isShow, setIsShow] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { user, isLoading, error } = useSelector((state) => state.user);
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(Login({ email, password }));
+  }
+  if (user) {
+    navigate('/');
   }
   return (
     <>
       <Helmet>
         <title>LiveTalk-login</title>
       </Helmet>
-      <div className='container'>
-        <form className='auth-form' onSubmit={handleLogin}>
-          <h3 className='heading'>login</h3>
-          <div className='field'>
-            <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)} required />
-          </div>
-          <div className='field password-field'>
-            {
-              isShow === false 
-              ? <BiSolidShow onClick={() => setIsShow(true)} className="password-icon" />
-              : <BiSolidHide onClick={() => setIsShow(false)} className="password-icon" />
-            }
-            <input type={`${isShow === false ? 'password' : 'text'}`} placeholder='password' onChange={(e) => setPassword(e.target.value)} required />
-          </div>
-          <input className='fullwidth' type='submit' value='login' />
-          <p>have no account on LiveTalk ? <Link to='/users/register'>register</Link></p>
-        </form>
-      </div>
+      {
+        isLoading === true ? <LoadingScreen /> :
+        <div className='container'>
+          {
+            error && (<ErrorBox message={'Invalid email or password'} />)
+          }
+          <form className='auth-form' onSubmit={handleLogin}>
+            <h3 className='heading'>login</h3>
+            <div className='field'>
+              <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+            <div className='field password-field'>
+              {
+                isShow === false 
+                ? <BiSolidShow onClick={() => setIsShow(true)} className="password-icon" />
+                : <BiSolidHide onClick={() => setIsShow(false)} className="password-icon" />
+              }
+              <input type={`${isShow === false ? 'password' : 'text'}`} placeholder='password' onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <input className='fullwidth' type='submit' value='login' />
+            <p>have no account on LiveTalk ? <Link to='/users/register'>register</Link></p>
+          </form>
+        </div>
+      }
     </>
   )
 }
--- frontend/src/pages/users/Register.jsx
+++ frontend/src/pages/users/Register.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { SignUp } from '../../features/UserFeatures';
 import { useAlert } from "react-alert";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import LoadingScreen from '../../components/LoadingScreen';
 
 const RegisterPage = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
+  const { user, isLoading } = useSelector((state) => state.user);
   const handleRegister = (e) => {
     e.preventDefault();
     if (firstName.length < 3 || firstName > 25) {
@@ -25,31 +28,37 @@ const RegisterPage = () => {
       dispatch(SignUp({ firstName, lastName, email, password }));
     }
   }
+  if (user) {
+    navigate('/');
+  }
   return (
     <>
       <Helmet>
         <title>LiveTalk-register</title>
       </Helmet>
-      <div className='container'>
-        <form className='auth-form' onSubmit={handleRegister}>
-          <h3 className='heading'>register</h3>
-          <div className='field multiple'>
-            <input type='text' placeholder='first name' onChange={(e) => setFirstName(e.target.value)} required />
-            <input type='text' placeholder='last name' onChange={(e) => setLastName(e.target.value)} required />
-          </div>
-          <div className='field'>
-            <input type='email' placeholder='email' onChange={(e) => setEmail(e.target.value)} required />
-          </div>
-          <div className='field'>
-            <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)} required />
-          </div>
-          <div className='field'>
-            <input type='password' placeholder='re enter password' onChange={(e) => setPasswordConfirm(e.target.value)} required />
-          </div>
-          <input className='fullwidth' type='submit' value='register' />
-          <p>have an account? <Link to='/users/login'>login</Link></p>
-        </form>
-      </div>
+      {
+        isLoading === true ? <LoadingScreen /> :
+        <div className='container'>
+          <form className='auth-form' onSubmit={handleRegister}>
+            <h3 className='heading'>register</h3>
+            <div className='field multiple'>
+              <input type='text' placeholder='first name' onChange={(e) => setFirstName(e.target.value)} required />
+              <input type='text' placeholder='last name' onChange={(e) => setLastName(e.target.value)} required />
+            </div>
+            <div className='field'>
+              <input type='email' placeholder='email' onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+            <div className='field'>
+              <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <div className='field'>
+              <input type='password' placeholder='re enter password' onChange={(e) => setPasswordConfirm(e.target.value)} required />
+            </div>
+            <input className='fullwidth' type='submit' value='register' />
+            <p>have an account? <Link to='/users/login'>login</Link></p>
+          </form>
+        </div>
+      }
     </>
   )
 }
