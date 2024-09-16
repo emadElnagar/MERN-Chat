ce0f08983c5d1eb1962d57e846771ec87dcad063
unknown <emad.m.alnagar@gmail.com>
2024-09-16 22:00:33 +0200

    Styling chat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,29 @@
-import './App.css';
-import NotFound from './pages/NotFound';
-import LoginPage from './pages/users/Login';
-import Profile from './pages/users/Profile';
-import RegisterPage from './pages/users/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import "./App.css";
+import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/users/Login";
+import Profile from "./pages/users/Profile";
+import RegisterPage from "./pages/users/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import { useSelector } from "react-redux";
+import ChatPage from "./pages/chat/chat";
 
 function App() {
   const { user } = useSelector((state) => state.user);
   return (
     <>
       <BrowserRouter>
-        {
-          user &&
-          <Header />
-        }
+        {user && <Header />}
         <Routes>
-          <Route path='/users/login' element={<LoginPage />} />
-          <Route path='/users/register' element={<RegisterPage />} />
-          <Route path='/users/profile/:id' element={<Profile />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/users/login" element={<LoginPage />} />
+          <Route path="/users/register" element={<RegisterPage />} />
+          <Route path="/users/profile/:id" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/chat/chat.jsx
@@ -0,0 +1,12 @@
+const ChatPage = () => {
+  return (
+    <div className="container">
+      <div className="chat-container">
+        <div className="chat-box"></div>
+        <div className="message-box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
