2d68c6b57f678dfef7065addc626f0705df45c72
unknown <emad.m.alnagar@gmail.com>
2024-09-22 23:00:25 +0200

    Must login to shwo chat page

--- frontend/src/pages/chat/chat.jsx
+++ frontend/src/pages/chat/chat.jsx
@@ -1,4 +1,12 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const ChatPage = () => {
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  if (!user) {
+    navigate("/users/login");
+  }
   return (
     <div className="container">
       <div className="chat-container">
