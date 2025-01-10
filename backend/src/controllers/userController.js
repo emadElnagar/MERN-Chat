b1686a2246515681a7d1c164bdd284534c659503
unknown <emad.m.alnagar@gmail.com>
2025-01-10 22:49:09 +0200

    Search user by first name and last name

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -114,7 +114,8 @@ export const SearchUser = async (req, res) => {
   const keyword = req.query.search
     ? {
         $or: [
-          { name: { $regex: req.query.search, $options: "i" } },
+          { firstName: { $regex: req.query.search, $options: "i" } },
+          { lastName: { $regex: req.query.search, $options: "i" } },
           { email: { $regex: req.query.search, $options: "i" } },
         ],
       }
