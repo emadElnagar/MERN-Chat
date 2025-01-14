6c544df838b14ffec12504b5959adfa8b40614df
unknown <emad.m.alnagar@gmail.com>
2025-01-14 21:54:52 +0200

    Chat page link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import reactLogo from '../assets/chat-icon.png';
+import LiveTalk from "../assets/chat-icon.png";
 import { AiFillMessage } from "react-icons/ai";
 import { FaUserAlt } from "react-icons/fa";
 import { IoSettingsSharp, IoSunnySharp } from "react-icons/io5";
 import { MdLogout } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { Logout } from '../features/UserFeatures';
+import { Logout } from "../features/UserFeatures";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -14,34 +14,50 @@ const Header = () => {
   // User logout
   const handleLogout = () => {
     dispatch(Logout());
-    navigate('/');
-  }
+    navigate("/");
+  };
   return (
     <header>
       <nav>
         <div>
-          <img src={reactLogo} className="logo" alt="Vite logo" />
+          <Link to={"/"}>
+            <img src={LiveTalk} className="logo" alt="Vite logo" />
+          </Link>
         </div>
         <div>
           <ul>
-            <li><AiFillMessage className='icon' title='chats' /></li>
             <li>
-              <Link to={`/users/profile/${user && user._id}`}>
-                <FaUserAlt className='icon' title='profile' />
+              <Link to={"/"}>
+                <AiFillMessage className="icon" title="chats" />
               </Link>
             </li>
-            <li><IoSettingsSharp className='icon' title='settings' /></li>
+            <li>
+              <Link to={`/users/profile/${user && user._id}`}>
+                <FaUserAlt className="icon" title="profile" />
+              </Link>
+            </li>
+            <li>
+              <IoSettingsSharp className="icon" title="settings" />
+            </li>
           </ul>
         </div>
-        <div className='user'>
+        <div className="user">
           <ul>
-            <li><IoSunnySharp className='icon' title='light' /></li>
-            <li><MdLogout className='icon' title='logout' onClick={handleLogout} /></li>
+            <li>
+              <IoSunnySharp className="icon" title="light" />
+            </li>
+            <li>
+              <MdLogout
+                className="icon"
+                title="logout"
+                onClick={handleLogout}
+              />
+            </li>
           </ul>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
