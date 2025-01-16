64ad52915785cad0a8b24be60b4ea485cc254d4d
unknown <emad.m.alnagar@gmail.com>
2025-01-16 22:43:17 +0200

    Change user password - Backend

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,20 +1,24 @@
-import User from '../models/User.js';
-import bcrypt from 'bcrypt';
-import JWT from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
 // Generate Token
 const generateToken = (user) => {
-  return JWT.sign({
-    _id: user._id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    userName: user.userName,
-    role: user.role
-  }, process.env.JWT_SECRET, {
-    expiresIn: '1h',
-  });
+  return JWT.sign(
+    {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
 };
 
 // User register
@@ -22,7 +26,7 @@ export const userRegister = async (req, res) => {
   const takenEmail = await User.findOne({ email: req.body.email });
   if (takenEmail) {
     res.status(401).json({
-      message: 'This email have already taken, please try another one'
+      message: "This email have already taken, please try another one",
     });
   }
   const user = new User({
@@ -32,31 +36,34 @@ export const userRegister = async (req, res) => {
     password: await bcrypt.hash(req.body.password, 10),
   });
   const token = generateToken(user);
-  await user.save().then(_user => {
-    res.status(200).json({
-      token
+  await user
+    .save()
+    .then((_user) => {
+      res.status(200).json({
+        token,
+      });
+    })
+    .catch((error) => {
+      res.status(201).json({
+        message: error.message,
+      });
     });
-  }).catch(error => {
-    res.status(201).json({
-      message: error.message
-    });
-  });
-}
+};
 
 // User login
 export const userLogin = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   if (user) {
-    if (bcrypt.compareSync(req.body.password, user.password)){
+    if (bcrypt.compareSync(req.body.password, user.password)) {
       const token = generateToken(user);
       res.status(200).json({
-        token
+        token,
       });
       return;
     }
   }
-  res.status(401).send({ message: 'invalid email or password' });
-}
+  res.status(401).send({ message: "invalid email or password" });
+};
 
 // Get all users
 export const GetAllUsers = async (_req, res) => {
@@ -65,10 +72,10 @@ export const GetAllUsers = async (_req, res) => {
     res.status(200).json(users);
   } catch (error) {
     res.status(401).json({
-      message: error.message
+      message: error.message,
     });
   }
-}
+};
 
 // Get single user
 export const GetSingleUser = async (req, res) => {
@@ -77,37 +84,69 @@ export const GetSingleUser = async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(401).json({
-      message: error.message
+      message: error.message,
     });
   }
-}
+};
+
+// Change user Password
+export const ChangePassword = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    const validate = await bcrypt.compare(
+      req.body.currentPassword,
+      user.password
+    );
+    if (!validate) {
+      res.status(401).json({
+        message: "Current password is not correct",
+      });
+    }
+    const newUser = { password: await bcrypt.hash(req.body.newPassword, 10) };
+    User.updateOne({ _id: req.params.id }, { $set: newUser })
+      .then((_result) => {
+        res.status(200).json({
+          message: "Password changed successfully",
+        });
+      })
+      .catch((error) => {
+        res.status(401).json({
+          message: error.message,
+        });
+      });
+  }
+};
 
 // Change user image
 export const ChangeUserImg = async (req, res) => {
-  const newUser = { image: req.file.filename }
-  User.updateOne({ _id: req.params.id }, { $set: newUser }).then(_result => {
-    res.status(200).json({
-      message: "User image changed successfully"
+  const newUser = { image: req.file.filename };
+  User.updateOne({ _id: req.params.id }, { $set: newUser })
+    .then((_result) => {
+      res.status(200).json({
+        message: "User image changed successfully",
+      });
+    })
+    .catch((error) => {
+      res.status(401).json({
+        message: error.message,
+      });
     });
-  }).catch((error) => {
-    res.status(401).json({
-      message: error.message
-    });
-  });
-}
+};
 
 // Delete user
 export const DeleteUser = async (req, res) => {
-  User.deleteOne({ _id: req.params.id }).then(_result => {
-    res.status(201).json({
-      message: "User Deleted Successfully"
+  User.deleteOne({ _id: req.params.id })
+    .then((_result) => {
+      res.status(201).json({
+        message: "User Deleted Successfully",
+      });
+    })
+    .catch((error) => {
+      res.status(401).json({
+        message: "Error Deleting User" + error.message,
+      });
     });
-  }).catch(error => {
-    res.status(401).json({
-      message: "Error Deleting User" + error.message
-    });
-  });
-}
+};
 
 // Search user
 export const SearchUser = async (req, res) => {
@@ -122,4 +161,4 @@ export const SearchUser = async (req, res) => {
     : {};
   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
   res.status(200).json({ users });
-}
+};
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,35 +1,40 @@
 import { Router } from "express";
 import upload from "../middlewares/multerMiddleWare.js";
-import { 
+import {
+  ChangePassword,
   ChangeUserImg,
   DeleteUser,
-  GetAllUsers, 
-  GetSingleUser, 
-  SearchUser, 
-  userLogin, 
-  userRegister 
+  GetAllUsers,
+  GetSingleUser,
+  SearchUser,
+  userLogin,
+  userRegister,
 } from "../controllers/userController.js";
 
 const userRouter = Router();
 
 // User register
-userRouter.post('/register', userRegister);
+userRouter.post("/register", userRegister);
 
 // User login
-userRouter.post('/login', userLogin);
+userRouter.post("/login", userLogin);
 
 // Get all users
-userRouter.get('/all', GetAllUsers);
+userRouter.get("/all", GetAllUsers);
 
 // Get single user
-userRouter.get('/profile/:id', GetSingleUser);
+userRouter.get("/profile/:id", GetSingleUser);
+
+// Change user password
+userRouter.post("/:id/resetpassword", ChangePassword);
 
 // Change user image
-userRouter.post('/:id/image/change', upload.single('file'), ChangeUserImg);
+userRouter.post("/:id/image/change", upload.single("file"), ChangeUserImg);
 
 // Delete user
-userRouter.delete('/:id/delete', DeleteUser);
+userRouter.delete("/:id/delete", DeleteUser);
 
-userRouter.get('/', SearchUser);
+// Search user
+userRouter.get("/", SearchUser);
 
 export default userRouter;
