7e27948f84cbac233ff1ba647ef2d231be064384
unknown <emad.m.alnagar@gmail.com>
2025-03-04 20:41:46 +0200

    Generate email in token

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -11,7 +11,7 @@ const generateToken = (user) => {
       _id: user._id,
       firstName: user.firstName,
       lastName: user.lastName,
-      userName: user.userName,
+      email: user.email,
       role: user.role,
     },
     process.env.JWT_SECRET,
