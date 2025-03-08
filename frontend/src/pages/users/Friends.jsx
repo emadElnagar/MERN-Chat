bbf1a0efca9bf750a068e71c6eff3c79a73706ea
unknown <emad.m.alnagar@gmail.com>
2025-03-08 19:57:10 +0200

    Display friends list

--- frontend/src/pages/users/Friends.jsx
+++ frontend/src/pages/users/Friends.jsx
@@ -1,10 +1,28 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Helmet } from "react-helmet";
 import { IoIosChatboxes, IoIosClose, IoIosCheckmark } from "react-icons/io";
 import { FaUserMinus } from "react-icons/fa";
+import { GetFriends } from "../../features/UserFeatures";
+import LoadingScreen from "../../components/LoadingScreen";
+import ErrorBox from "../../components/ErrorBox";
+import UserAvatar from "../../assets/user-avatar.png";
 
 const FriendsPage = () => {
+  const { user, error, isLoading, friendsList } = useSelector(
+    (state) => state.user
+  );
   const [tab, setTab] = useState("friends");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  if (!user) {
+    navigate("/login");
+  }
+  // Get friends list
+  useEffect(() => {
+    dispatch(GetFriends());
+  }, [dispatch]);
   return (
     <Fragment>
       <Helmet>
@@ -32,75 +50,108 @@ const FriendsPage = () => {
           </button>
         </div>
         <div className="users-list">
-          {tab === "friends" && (
+          {isLoading ? (
+            <LoadingScreen />
+          ) : error ? (
+            <ErrorBox message={error} />
+          ) : (
             <ul>
-              <li>
-                <div className="user image">
-                  <img
-                    src="https://www.pngarts.com/files/5/User-Avatar-PNG-Transparent-Image.png"
-                    alt="problem showing image"
-                  />
-                </div>
-                <div className="user-name">
-                  <h4>John Doe</h4>
-                </div>
-                <div className="button">
-                  <button>
-                    Chat <IoIosChatboxes />
-                  </button>
-                </div>
-              </li>
-            </ul>
-          )}
-          {tab === "requests" && (
-            <ul>
-              {tab === "requests" && (
-                <ul>
-                  <li>
-                    <div className="user image">
-                      <img
-                        src="https://www.pngarts.com/files/5/User-Avatar-PNG-Transparent-Image.png"
-                        alt="problem showing image"
-                      />
-                    </div>
-                    <div className="user-name">
-                      <h4>John Doe</h4>
-                    </div>
-                    <div className="button">
-                      <button>
-                        Accept <IoIosCheckmark />
-                      </button>
-                      <button>
-                        Decline <IoIosClose />
-                      </button>
-                    </div>
-                  </li>
-                </ul>
-              )}
-            </ul>
-          )}
-          {tab === "sent" && (
-            <ul>
-              {tab === "sent" && (
-                <ul>
-                  <li>
-                    <div className="user image">
-                      <img
-                        src="https://www.pngarts.com/files/5/User-Avatar-PNG-Transparent-Image.png"
-                        alt="problem showing image"
-                      />
-                    </div>
-                    <div className="user-name">
-                      <h4>John Doe</h4>
-                    </div>
-                    <div className="button">
-                      <button>
-                        Cancel request <FaUserMinus />
-                      </button>
-                    </div>
-                  </li>
-                </ul>
-              )}
+              {tab === "friends" &&
+                (friendsList &&
+                friendsList.friends &&
+                friendsList.friends.length > 0 ? (
+                  friendsList.friends.map((friend, index) => (
+                    <li key={index}>
+                      <div className="user image">
+                        <img
+                          src={`${
+                            friend.image
+                              ? "http://localhost:5000/" + friend.image
+                              : UserAvatar
+                          }`}
+                          alt="problem showing image"
+                        />
+                      </div>
+                      <div className="user-name">
+                        <h4>
+                          {friend.firstName} {friend.lastName}
+                        </h4>
+                      </div>
+                      <div className="button">
+                        <button>
+                          Chat <IoIosChatboxes />
+                        </button>
+                      </div>
+                    </li>
+                  ))
+                ) : (
+                  <h4>No friends yet</h4>
+                ))}
+              {tab === "requests" &&
+                (friendsList &&
+                friendsList.friendRequests &&
+                friendsList.friendRequests.length > 0 ? (
+                  friendsList.friendRequests.map((friend, index) => (
+                    <li key={index}>
+                      <div className="user image">
+                        <img
+                          src={`${
+                            friend.image
+                              ? "http://localhost:5000/" + friend.image
+                              : UserAvatar
+                          }`}
+                          alt="problem showing image"
+                        />
+                      </div>
+                      <div className="user-name">
+                        <h4>
+                          {friend.firstName} {friend.lastName}
+                        </h4>
+                      </div>
+                      <div className="button">
+                        <button>
+                          Accept <IoIosCheckmark />
+                        </button>
+                        <button>
+                          Decline <IoIosClose />
+                        </button>
+                      </div>
+                    </li>
+                  ))
+                ) : (
+                  <h4>No friend requests found</h4>
+                ))}
+              {tab === "sent" &&
+                (friendsList &&
+                friendsList.sentRequests &&
+                friendsList.sentRequests.length > 0 ? (
+                  friendsList.sentRequests.map((friend, index) => (
+                    <li key={index}>
+                      <div className="user image">
+                        <img
+                          src={`${
+                            friend.image
+                              ? "http://localhost:5000/" + friend.image
+                              : UserAvatar
+                          }`}
+                          alt="problem showing image"
+                        />
+                      </div>
+                      <div className="user-name">
+                        <h4>
+                          {friend.firstName} {friend.lastName}
+                        </h4>
+                      </div>
+                      <div className="button">
+                        <button>
+                          Cancel request <FaUserMinus />
+                        </button>
+                      </div>
+                    </li>
+                  ))
+                ) : (
+                  <h4>No sent requests found</h4>
+                ))}
             </ul>
           )}
         </div>
