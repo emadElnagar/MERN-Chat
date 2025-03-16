edfaf4ec04acfeab6ffe774d9cb28c683cd85162
unknown <emad.m.alnagar@gmail.com>
2025-03-16 21:16:15 +0200

    Protect create new chat route

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createChat } from "../controllers/chatController.js";
+import { isAuth } from "../middlewares/authMiddleWare.js";
 
 const chatRouter = Router();
 
 // Create a new chat
-chatRouter.post("/", createChat);
+chatRouter.post("/", isAuth, createChat);
 
 export default chatRouter;
