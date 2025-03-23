cc9a43004efac9ce24a84c29ed30d6756fd82f9d
unknown <emad.m.alnagar@gmail.com>
2025-03-23 21:48:03 +0200

    Default value for chat name

--- backend/src/models/Chat.js
+++ backend/src/models/Chat.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const chatSchema = mongoose.Schema(
   {
-    chatName: { type: String, trim: true },
+    chatName: {
+      type: String,
+      trim: true,
+      default: function () {
+        return this.user.map((user) => user.name).join(",");
+      },
+    },
     isGroupChat: { type: Boolean, default: false },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     messages: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
