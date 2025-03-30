032094615d7af7b0de9d31933f50f2f8a73fcbda
unknown <emad.m.alnagar@gmail.com>
2025-03-30 22:43:21 +0200

    Fetch chats feature

--- frontend/src/features/ChatFeatures.js
+++ frontend/src/features/ChatFeatures.js
@@ -10,11 +10,43 @@ const initialState = {
   error: null,
 };
 
+export const FetchChats = createAsyncThunk(
+  "chat/FetchChats",
+  async (_, { rejectWithValue }) => {
+    try {
+      const token = sessionStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.get(`${url}`, config);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const chatSlice = createSlice({
   name: "chat",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(FetchChats.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(FetchChats.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.chats = action.payload;
+      })
+      .addCase(FetchChats.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error;
+      });
+  },
 });
 
 export default chatSlice.reducer;
