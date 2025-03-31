d096c534dc9b439ce5d888c3fe0760e928f31d00
unknown <emad.m.alnagar@gmail.com>
2025-03-31 21:16:32 +0200

    Get single chat

--- frontend/src/features/ChatFeatures.js
+++ frontend/src/features/ChatFeatures.js
@@ -10,6 +10,7 @@ const initialState = {
   error: null,
 };
 
+// Get user chats
 export const FetchChats = createAsyncThunk(
   "chat/FetchChats",
   async (_, { rejectWithValue }) => {
@@ -28,12 +29,32 @@ export const FetchChats = createAsyncThunk(
   }
 );
 
+// Get single chat
+export const FetchSingleChat = createAsyncThunk(
+  "chat/FetchSingleChat",
+  async (chatId, { rejectWithValue }) => {
+    try {
+      const token = sessionStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.get(`${url}/${chatId}`, config);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const chatSlice = createSlice({
   name: "chat",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // Fetch chats
       .addCase(FetchChats.pending, (state) => {
         state.isLoading = true;
         state.error = null;
@@ -43,6 +64,19 @@ const chatSlice = createSlice({
         state.chats = action.payload;
       })
       .addCase(FetchChats.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error;
+      })
+      // Fetch single chat
+      .addCase(FetchSingleChat.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(FetchSingleChat.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.chat = action.payload;
+      })
+      .addCase(FetchSingleChat.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error;
       });
