6a1fb9c916217d6cad7a06236945d5b51cf7ef20
unknown <emad.m.alnagar@gmail.com>
2025-04-01 18:05:03 +0200

    Current message ui

--- frontend/src/pages/chat/chat.jsx
+++ frontend/src/pages/chat/chat.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { IoSend } from "react-icons/io5";
 
 const ChatPage = () => {
   const { user } = useSelector((state) => state.user);
@@ -27,7 +28,26 @@ const ChatPage = () => {
               </div>
             </div>
           </div>
-          <div className="current-chat"></div>
+          <div className="current-chat">
+            <div className="chat-window">
+              <div className="message-received">
+                <img src="https://placehold.co/50x50" alt="User" />
+                <p className="message-content">Hello</p>
+              </div>
+              <div className="message-sent">
+                <p className="message-content">Hi there</p>
+                <span className="message-status">seen</span>
+              </div>
+            </div>
+            <div className="chat-form">
+              <form>
+                <input type="text" placeholder="Type a message..." />
+                <button>
+                  <IoSend />
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
