d468a0650442a2a6c3aae914c370afe6b311a4e9
unknown <emad.m.alnagar@gmail.com>
2025-05-27 23:24:19 +0200

    Is admin middleware

--- backend/src/middlewares/authMiddleWare.js
+++ backend/src/middlewares/authMiddleWare.js
@@ -22,3 +22,12 @@ export const isAuth = async (req, res, next) => {
     throw new Error("Not authorized, no token");
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403);
+    throw new Error("Not authorized as an admin");
+  }
+};
