3676d8f8534f8d1a9ac1b586ca3986e76300d5ce
unknown <emad.m.alnagar@gmail.com>
2025-10-14 23:05:24 +0200

    limit size request

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads"));
 
