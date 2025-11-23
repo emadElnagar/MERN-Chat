035aa19d83a729c7a26bc28bf607a7ee216b0f54
unknown <emad.m.alnagar@gmail.com>
2025-11-23 23:11:45 +0200

    is auth middleware

--- backend/src/middlewares/authMiddleWare.js
+++ backend/src/middlewares/authMiddleWare.js
@@ -28,26 +28,21 @@ export const generateRefreshToken = (user) => {
   );
 };
 
-export const isAuth = async (req, res, next) => {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded._id).select("-password");
-      next();
-    } catch (error) {
-      res.status(401);
-      throw new Error("Not authorized, token failed");
-    }
+export const isAuth = (req, res, next) => {
+  let authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Not authorized" });
   }
-  if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
-  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: "Token expired" });
+
+    req.user = decoded;
+    next();
+  });
 };
 
 export const isAdmin = (req, res, next) => {
